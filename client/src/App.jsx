0c3f2cfbae3f3fc4232360922903b73ba11ff395
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-09-27 06:51:16 +0200

    feat: added routes for home, signin and signup and a navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,70 +1,28 @@
-import { useState } from 'react';
-import axios from 'axios';
+// import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
+import SignInPage from './Components/SignInPage';
+import SignUpPage from './Components/SignUpPage';
+import HomePage from './Components/HomePage';
+import ResponsiveAppBar from './Components/ResponsiveAppBar';
+import ResponsiveAppBarAuth from './Components/ResponsiveAppBarAuth';
 
 function App() {
-	const [formData, setFormData] = useState({
-		name: '',
-		username: '',
-		password: ''
-	});
-
-
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData((prevData) => ({
-			...prevData,
-			[name]: value,
-		}));
-	};
-
-
-
-	const handleRegister = async (e) => {
-		e.preventDefault();
-		try {
-			const res = await axios.post('http://localhost:5000/api/auth/register',
-				formData,
-				// { withCredentials: true }
-			);
-			console.log('User registered:', res.data);
-		} catch (err) {
-			console.error('Error:', err.response.data);
-		}
-	};
-
-	const handleLogin = async (e) => {
-		e.preventDefault();
-		try {
-			const res = await axios.post('http://localhost:5000/api/auth/login', {
-				username: formData.username,
-				password: formData.password
-				// }, { withCredentials: true });
-			})
-			console.log(res.data);
-		} catch (err) {
-			console.error(err.response.data);
-		}
-	};
 
 	return (
-		<div className="App">
-			<h1>Register</h1>
-			<form onSubmit={handleRegister}>
-				<input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required />
-				{/* <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required /> */}
-				<input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} required />
-				<input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
-				<button type="submit">Register</button>
-			</form>
+		<>
+			<BrowserRouter>
+				{/* <Header /> */}
+				{/* <ResponsiveAppBar /> */}
+				<ResponsiveAppBarAuth />
 
-			<h1>Login</h1>
-			<form onSubmit={handleLogin}>
-				{/* <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required /> */}
-				<input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} required />
-				<input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
-				<button type="submit">Login</button>
-			</form>
-		</div>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/signin" element={<SignInPage />} />
+					<Route path="/signup" element={<SignUpPage />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
