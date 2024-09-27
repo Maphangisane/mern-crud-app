740dde1b279ebfeba207bd63c33d686eb51cc2a3
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-09-27 22:11:45 +0200

    update: check if user exist before signup and error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,17 +9,22 @@ exports.register = async (req, res) => {
 	try {
 		const { name, username, password } = req.body;
 
-		// Validate request body
 		if (!name || !username || !password) {
 			return res.status(400).json({ error: 'name, username and password are required' });
 		}
 
+		const existingUser = await User.findOne({ username });
+		if (existingUser) {
+			return res.status(400).json({ error: 'Username is already taken' });
+		}
+
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
-		const user = new User({ name, username, password: hashedPassword });
 
+		const user = new User({ name, username, password: hashedPassword });
 		await user.save();
-		res.status(201).send('User registered');
+
+		res.status(201).json({ message: 'User signed up successfully' });
 	} catch (error) {
 		res.status(500).json({ error: 'Server error: User failed to register' });
 	}
@@ -27,13 +32,23 @@ exports.register = async (req, res) => {
 
 // Login a user
 exports.login = async (req, res) => {
-	const { username, password } = req.body;
-	const user = await User.findOne({ username });
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
 
-	if (user && (await bcrypt.compare(password, user.password))) {
-		const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET, { expiresIn: '1h' });
-		res.json({ token });
-	} else {
-		res.status(401).send('Invalid credentials');
+		if (user && (await bcrypt.compare(password, user.password))) {
+			const access_token = jwt.sign(
+				{ id: user._id, username: user.username },
+				JWT_SECRET,
+				{ expiresIn: '1h' }
+			);
+
+			res.json({ access_token });
+		} else {
+			res.status(401).json({ message: 'Invalid credentials' }); // json
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Server error: Failed to login' });
 	}
 };
