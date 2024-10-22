d4bfb3f2ea89a14c8d089ffbcd3e5f5cc44a1844
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-10-22 13:29:49 +0200

    add: notes component

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
-
+import Notes from '../Components/Notes'
 
 function HomePage() {
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 	return (
 		<div>
 			<Container>
 				<Typography variant="h4" gutterBottom style={{ marginTop: '20px' }}>
 					Welcome to MERN CRUD with Material-UI
+					<Notes />
 				</Typography>
 
 			</Container>
