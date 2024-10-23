5d59897af0980ae86bf6501909439ef0e836b540
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-10-23 15:57:05 +0200

    add: toggle theme button on navbar

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import { isLoggedIn } from '/src/utils/auth.js';
 import ResponsiveAppBar from './ResponsiveAppBar';
 import ResponsiveAppBarAuth from './ResponsiveAppBarAuth';
-
+import ThemeToggleButton from './ThemeToggleButton';
 
 function Header() {
 	// Get login status
@@ -14,6 +14,7 @@ function Header() {
 		<div>
 			<Toolbar>
 				{loggedIn ? <ResponsiveAppBar /> : <ResponsiveAppBarAuth />}
+				<ThemeToggleButton />
 			</Toolbar>
 
 		</div>
