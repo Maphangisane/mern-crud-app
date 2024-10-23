8c17c0156eb2403061f716517f17d191c23f6378
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-10-23 21:01:16 +0200

    feat: user authentication handler to provide username, auth status, login and logout implementations

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,49 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+	const [authToken, setAuthToken] = useState(null);
+	const [userName, setUserName] = useState(null);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	const login = async (username, password) => {
+		const requestBody = { username, password };
+
+		const res = await axios.post('http://localhost:5000/api/auth/login', requestBody, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		const { access_token, name } = res.data;
+
+		if (access_token) {
+			setAuthToken(access_token);
+			setUserName(name);
+			setIsAuthenticated(true);
+			axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+			localStorage.setItem("authToken", access_token);
+			localStorage.setItem("userName", name);
+		} else {
+			throw new Error('Invalid username or password');
+		}
+	};
+
+	const logout = () => {
+		setAuthToken(null);
+		setUserName(null);
+		setIsAuthenticated(false);
+		localStorage.removeItem("authToken");
+		localStorage.removeItem("userName");
+		delete axios.defaults.headers.common['Authorization'];
+	};
+
+
+	return (
+		<AuthContext.Provider value={{ isAuthenticated, userName, login, logout }}>
+			{children}
+		</AuthContext.Provider>
+	);
+}
