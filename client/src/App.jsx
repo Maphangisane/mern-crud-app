b2454d5ec78c3ac8849369c9dfdf4bcc17b19ecb
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-10-23 15:55:20 +0200

    add: ThemeProvider use context wrapper and not found route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,42 @@
 // import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
 import Header from "./Components/Header";
 import SignInPage from './Components/SignInPage';
 import SignUpPage from './Components/SignUpPage';
 import HomePage from './Components/HomePage';
 import Footer from './Components/Footer';
 import PrivateRoutes from './Components/PrivateRoutes';
+import NotFound from './Components/NotFound';
 
 function App() {
 
 	return (
 		<>
-			<BrowserRouter>
-				<Header />
+			<ThemeProvider>
+				<BrowserRouter>
+					<Header />
 
-				<Routes>
-					{/* Public routes */}
-					<Route path="/signin" element={<SignInPage />} />
-					<Route path="/signup" element={<SignUpPage />} />
+					<Routes>
+						{/* Public routes */}
+						<Route path="/signin" element={<SignInPage />} />
+						<Route path="/signup" element={<SignUpPage />} />
 
-					{/* Protected routes*/}
-					<Route
-						path="/"
-						element={
-							<PrivateRoutes>
-								<HomePage />
-							</PrivateRoutes>
-						}
-					/>
-				</Routes>
+						{/* Protected routes*/}
+						<Route
+							path="/"
+							element={
+								<PrivateRoutes>
+									<HomePage />
+								</PrivateRoutes>
+							}
+						/>
+						<Route path="*" element={<NotFound />} />
+					</Routes>
 
-				<Footer />
-			</BrowserRouter>
+					<Footer />
+				</BrowserRouter>
+			</ThemeProvider>
 		</>
 	);
 }
