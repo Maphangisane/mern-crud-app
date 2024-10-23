dad05f2464073bd5a0600d28ef48c7600752b23e
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-10-23 16:00:04 +0200

    add: styling for theme toggling

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import Notes from '../Components/Notes'
+import { useTheme } from '../contexts/ThemeContext';
 
 function HomePage() {
 	// const navigate = useNavigate();
+	const { theme } = useTheme(); // Access the theme
+
+	const containerStyle = {
+		backgroundColor: theme.palette.background,
+		color: theme.palette.text,
+		minHeight: '100vh',
+		padding: '20px',
+	}
+
 	const userName = localStorage.getItem("userName");
 	return (
-		<div>
+		<div style={containerStyle}>
 			<Container>
 				<Typography variant="h4" gutterBottom style={{ marginTop: '20px' }}>
 					Welcome {userName}
