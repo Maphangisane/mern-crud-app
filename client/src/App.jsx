1abe437698512cf5ae23cbc6fb3652eb476cdd76
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-10-24 02:26:26 +0200

    add: auth and search provider, protected routes, conditional public routes and invalid routes handler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 // import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// contexts
 import { ThemeProvider } from './contexts/ThemeContext';
+import { AuthProvider, AuthContext } from './contexts/AuthContext';
+import { SearchProvider } from './contexts/SearchContext';
+// components
 import Header from "./Components/Header";
 import SignInPage from './Components/SignInPage';
 import SignUpPage from './Components/SignUpPage';
@@ -8,35 +13,52 @@ import HomePage from './Components/HomePage';
 import Footer from './Components/Footer';
 import PrivateRoutes from './Components/PrivateRoutes';
 import NotFound from './Components/NotFound';
+import { isLoggedIn } from './utils/auth';
 
 function App() {
+	// const { isAuthenticated } = useContext(AuthContext);
+	const isAuthenticated = isLoggedIn();
 
 	return (
 		<>
-			<ThemeProvider>
-				<BrowserRouter>
-					<Header />
+			<AuthProvider>
+				<SearchProvider>
+					<ThemeProvider>
+						<BrowserRouter>
+							<Header />
 
-					<Routes>
-						{/* Public routes */}
-						<Route path="/signin" element={<SignInPage />} />
-						<Route path="/signup" element={<SignUpPage />} />
+							<Routes>
+								{/* Public routes */}
+								<Route
+									path="/signin"
+									element={
+										isAuthenticated ? <Navigate to="/" /> : <SignInPage />
+									}
+								/>
+								<Route
+									path="/signup"
+									element={
+										isAuthenticated ? <Navigate to="/" /> : <SignUpPage />
+									}
+								/>
 
-						{/* Protected routes*/}
-						<Route
-							path="/"
-							element={
-								<PrivateRoutes>
-									<HomePage />
-								</PrivateRoutes>
-							}
-						/>
-						<Route path="*" element={<NotFound />} />
-					</Routes>
+								{/* Protected routes */}
+								<Route
+									path="/"
+									element={
+										<PrivateRoutes>
+											<HomePage />
+										</PrivateRoutes>
+									}
+								/>
+								<Route path="*" element={<NotFound />} />
+							</Routes>
 
-					<Footer />
-				</BrowserRouter>
-			</ThemeProvider>
+							<Footer />
+						</BrowserRouter>
+					</ThemeProvider>
+				</SearchProvider>
+			</AuthProvider>
 		</>
 	);
 }
