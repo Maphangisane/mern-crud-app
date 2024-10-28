92f8b381fa04cf97fbdc5d4686486cf252bf43ee
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-10-28 18:08:42 +0200

    add: setup proxy server for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// server: {
-	// 	host: '127.0.0.1',  //binds to IPv4 localhost
-	// 	port: 3000,  // custon port
-	// 	open: false // open the browser on server start
-	// }
-})
+	server: {
+		// host: '127.0.0.1',  //binds to IPv4 localhost
+		host: '0.0.0.0',  // Allows access from external IPs
+		port: 5173,  // custom port
+		open: false, // open the browser on server start
+		proxy: {
+			// Proxy API requests to backend server
+			'/api': {
+				// target: 'http://172.105.93.26:5000', // Backend server address
+				target: 'http://localhost:5000',
+				changeOrigin: true,
+				secure: false,
+				// rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	}
+});
